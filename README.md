a0b477938157b2bbb7f9257fd296ea5b53764e27
Shanti Rodríguez <srodriguez.bit@gmail.com>
2015-02-01 13:49:37 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # Integrate PayPal SDK Into Laravel 4
+
 Integrate PayPal SDK Into Laravel 4
+
+## Install PayPal SDK via composer
+
+Edit file composer.json
+
+{
+    ...
+  "require": {
+        ...
+      "paypal/rest-api-sdk-php": "*"
+  },
+    ...
+}
+
+Update the dependencies
+
+*$ php composer.phar update --no-dev*
+
+## Configure PayPal credential
+
+app/config/paypal.php
+
+'client_id' => '',
+'secret' => '',
+
+
+
---
+++ paypal/app/routes.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+Route::get('/', function()
+{
+	return View::make('hello');
+});
+
+/*
+PAYPAL
+*/
+
+Route::resource('pay', 'IndexController');
+
+Route::post('payment', array(
+    'as' => 'payment',
+    'uses' => 'IndexController@postPayment',
+));
+
+// this is after make the payment, PayPal redirect back to your site
+Route::get('payment/status', array(
+    'as' => 'payment.status',
+    'uses' => 'IndexController@getPaymentStatus',
+));
